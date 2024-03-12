fbb2a32dbfa53c467ea56b272e1fb986258ac281
vuthehuynh <vuthe_huynh@yahoo.com>
2024-03-12 15:29:01 +0700

    change port

--- frontend/settings.py
+++ frontend/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    FRONENT_PORT: str = "8000"
+    FRONENT_PORT: str = "5000"
     MODE: str = "production" # production, debug
 
 settings = Settings()
