b0f19304db9f5059d5379504365ed615a5e8e095
vuthehuynh <vuthe_huynh@yahoo.com>
2024-03-12 14:21:14 +0700

    fix requirement installation

--- frontend/settings.py
+++ frontend/settings.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Union
 
-from pydantic import AnyHttpUrl, BaseSettings, HttpUrl, validator
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
