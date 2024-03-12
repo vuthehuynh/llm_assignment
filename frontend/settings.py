c7dad8d9d6c8552544c81bc4e7eaae7ad9cae8a2
vuthehuynh <vuthe_huynh@yahoo.com>
2024-03-12 14:05:03 +0700

    change docker run file fronend

--- frontend/settings.py
+++ frontend/settings.py
@@ -5,6 +5,6 @@ from pydantic import AnyHttpUrl, BaseSettings, HttpUrl, validator
 
 class Settings(BaseSettings):
     FRONENT_PORT: str = "8000"
-    MODE: str = "debug" # DEVELOPMENT, PRODUCTION
+    MODE: str = "production" # production, debug
 
 settings = Settings()
